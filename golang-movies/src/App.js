dd6c858ab9190495a18878b1220e50f54573f02b
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-10 06:43:53 -0800

    add movie catgories

--- golang-movies/src/App.js
+++ golang-movies/src/App.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom'
 import Movies from './components/Movies';
 import Home from './components/Home';
 import Admin from './components/Admin';
+import Categories from './components/Categories';
 
 export default function App() {
   return (
@@ -26,6 +27,9 @@ export default function App() {
                 <Link to="/movies">Movies</Link>
               </li>
               <li className="list-group-item">
+                <Link to="/by-category">Category</Link>
+              </li>
+              <li className="list-group-item">
                 <Link to="/admin">Manage Catalog</Link>
               </li>
             </ul> 
@@ -39,6 +43,22 @@ export default function App() {
             <Route path="/movies">
               <Movies />
             </Route>
+            <Route exact path="/by-category">
+              <CategoryPage />
+            </Route>
+
+            <Route 
+            exact 
+            path="/by-category/drama"
+            render={(props) => <Categories {...props} title={`Drama`} />}
+            />
+
+            <Route 
+            exact 
+            path="/by-category/comedy"
+            render={(props) => <Categories {...props} title={`Comedy`} />}
+            />
+
             <Route path="/admin">
               <Admin />
             </Route>
@@ -57,4 +77,18 @@ export default function App() {
 function Movie() {
   let { id } = useParams();
   return <h2>Movie id {id}</h2>
+}
+
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        <li><Link to={`${path}/comedy`}>Comedy</Link></li>
+        <li><Link to={`${path}/drama`}>Drama</Link></li>
+      </ul>
+    </div>
+  );
 }
