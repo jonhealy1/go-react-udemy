37b81392fb5c7cfc0a45fd278f8af98a65234771
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-10 22:20:02 -0800

    start main

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -1,8 +1,21 @@
 package main
 
+import "flag"
+import "fmt"
+
 const version = "1.0.0"
 
-type = config struct {
+type config struct {
 	port int
 	env string
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listne on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
+	flag.Parse()
+
+	fmt.Println("Running")
 }
