82bed70e58aa8e94cf2abc9f94ce026d01b06a13
jonhealy1 <jonathan.d.healy@gmail.com>
2022-02-11 02:29:00 -0800

    return json

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -4,12 +4,19 @@ import "flag"
 import "fmt"
 import "net/http"
 import "log"
+import "encoding/json"
 
 const version = "1.0.0"
 
 type config struct {
 	port int
 	env string
+}
+
+type AppStatus struct {
+	Status string
+	Environment string
+	Version string
 }
 
 func main() {
@@ -22,7 +29,20 @@ func main() {
 	fmt.Println("Running")
 
 	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "status")
+		currentStatus := AppStatus{
+			Status: "Available",
+			Environment: cfg.env,
+			Version: version,
+		}
+		js, err := json.MarshalIndent(currentStatus, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(js)
+
 	})
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
